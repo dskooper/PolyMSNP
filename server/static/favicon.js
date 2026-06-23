bc751e400a66df8f6a41130337b0d6c212e9a6a9
kooper! <ds.kooper@outlook.com>
2026-06-23 19:51:40 +0100

    Update favicon.js??

--- server/static/favicon.js
+++ server/static/favicon.js
@@ -2,5 +2,5 @@ const favicon = document.getElementById("favicon");
 const month = new Date().getMonth() + 1;
         
 if ( month === 6 ) {
-	favicon.href = "/imsnp-favicon-pride.png"
+	favicon.href = "/imsnp-favicon-pride.png";
 }
