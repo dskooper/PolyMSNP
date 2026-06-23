f13b1cffeb566bd46b8a203b07c6907312130049
kooper! <ds.kooper@outlook.com>
2026-06-23 19:19:36 +0100

    js file for dynamic favicon replacement

---
+++ server/static/favicon.js
@@ -0,0 +1,6 @@
+const favicon = document.getElementById("favicon");
+const month = new Date().getMonth() + 1;
+        
+if ( month === 6 ) {
+	favicon.href = "https://github.com/dskooper/PolyMSNP/raw/refs/heads/main/server/static/imsnp-favicon-pride.png"
+}
