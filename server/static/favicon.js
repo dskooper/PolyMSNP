1a741e6112bb8b37b2d86ae1d0745a2b2514bec2
kooper! <ds.kooper@outlook.com>
2026-06-23 19:21:30 +0100

    dumbass

--- server/static/favicon.js
+++ server/static/favicon.js
@@ -2,5 +2,5 @@ const favicon = document.getElementById("favicon");
 const month = new Date().getMonth() + 1;
         
 if ( month === 6 ) {
-	favicon.href = "https://github.com/dskooper/PolyMSNP/raw/refs/heads/main/server/static/imsnp-favicon-pride.png"
+	favicon.href = "/imsnp-favicon-pride.png"
 }
